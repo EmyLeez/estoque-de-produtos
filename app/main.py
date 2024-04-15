aabc9162024ca4789feef04aa424991bfdbd722a
Emylly-Guimaraes <emylly.guimaraes@furg.br>
2024-04-14 23:47:57 -0300

    Adicionando o esqueleto incial do sistema

---
+++ app/main.py
@@ -0,0 +1,13 @@
+def estoque():
+    print("Bem-vindo ao sistema de estoque")
+    
+    while True:
+        print("Selecione um número de acordo com seu interesse:")
+        print("1 - Cadastrar novo produto ")
+        print("2 - Listar todos os produtos ")
+        print("3 - Sair ")
+        p1 = int(input("Digite o número:"))
+
+estoque()
+
+
