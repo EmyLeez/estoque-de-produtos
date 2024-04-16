c74cd2c400e534cc894041848e2981bccbee740f
Emylly-Guimaraes <emylly.guimaraes@furg.br>
2024-04-16 00:01:38 -0300

    Adicionando funcionalidades no projeto

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def estoque():
     print("Bem-vindo ao sistema de estoque\n")
     
+    lista1 = []
+    dicionario1 = {
+        'nome': f'{nome_do_produto}',
+        'preco': f'{preco}',
+        'quant': f'{qtd}'
+    }
+
     while True:
         print("Selecione um número de acordo com seu interesse:\n")
         print("1 - Cadastrar novo produto ")
@@ -11,7 +18,14 @@ def estoque():
         if opcao1 == 3:
             print("Obrigada por usar nosso sistema!")
             return 'Sistema encerrado'
-
+        elif opcao1 == 1:
+            input("Cadastre o novo produto:")
+            return 'Cadastrado com sucesso!'
+        elif opcao1 == 2:
+            print("Produtos cadastrados:")
+            print(opcao1)
+            return '!'
+            
 
 print(estoque())
 
---
+++ teste.py
@@ -0,0 +1,13 @@
+
+nome_do_produto = 'algo'
+preco = 'oh'
+qtd = 'chato'
+
+dicionario1 = {
+        'nome': f'{nome_do_produto}',
+        'preco': f'{preco}',
+        'quant': f'{qtd}'
+    }
+
+print(dicionario1)
+
