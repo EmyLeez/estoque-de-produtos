d6146f2b5548e588df04fd18a96149ab7503666b
Emylly-Guimaraes <emylly.guimaraes@furg.br>
2024-04-16 23:22:44 -0300

    Testando funcionalidade

--- teste.py
+++ teste.py
@@ -11,3 +11,15 @@ dicionario1 = {
 
 print(dicionario1)
 
+nome_do_produto = 'algo'
+preco = 'oh'
+qtd = 'chato'
+
+dicionario1 = {
+        'nome': f'{nome_do_produto}',
+        'preco': f'{preco}',
+        'quant': f'{qtd}'
+    }
+
+print(dicionario1)
+
